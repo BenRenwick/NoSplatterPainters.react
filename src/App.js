cacd138e263182129b975035069f3c0302750347
Katrina <krenwick@outlook.com>
2023-05-30 21:28:43 -0400

    add contact info

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
     <div className="App">
       <div className="hero-container">
         <h1>Ben Paints</h1>
+
+        <div className="contact-info">
+          <a href="mailto: ben@gmail.com">ben@gmail.com</a>
+          <br />
+          <a href="tel:+16135619258">(613) 561 9258</a>
+        </div>
       </div>
     </div>
   );
