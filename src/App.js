b065e70bc37d76534c80e8d74b7133cc9cc9b7f9
Katrina <krenwick@outlook.com>
2023-05-30 21:14:27 -0400

    add hero image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h1>Ben Paints</h1>
+      <div className="hero-container">
+        <h1>Ben Paints</h1>
+      </div>
     </div>
   );
 }
